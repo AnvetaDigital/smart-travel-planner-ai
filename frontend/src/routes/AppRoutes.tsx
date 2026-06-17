cfdc25826ce990b18eec3d5f6f60c0b1da526aa8
Anveta <nangarea2017@gmail.com>
2026-06-17 17:52:14 +0530

    [feat]: add route for trip summary page

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -6,6 +6,7 @@ import HomePage from "@/pages/HomePage";
 import LoginPage from "@/pages/LoginPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import SignupPage from "@/pages/SignupPage";
+import TripSummary from "@/pages/TripSummary";
 import { Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
@@ -18,6 +19,7 @@ export default function AppRoutes() {
     <Route path="/dashboard" element={<Dashboard/>}/>
     <Route path="/trips/create" element={<CreateTrip/>}/>
     <Route path="/chat" element={<ChatPage/>}/>
+    <Route path="/trip-summary" element={<TripSummary/>} />
     </Route>
     <Route path="*" element={<NotFoundPage/>}/>
   </Routes>
