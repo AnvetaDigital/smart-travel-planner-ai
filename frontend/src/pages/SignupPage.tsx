fbb2bb0d7fa629393ab85ad0bb3cf9925d4e8902
Anveta <nangarea2017@gmail.com>
2026-06-17 17:51:30 +0530

    [feat]: build signup page

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,8 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function SignupPage() {
   return (
-    <div>
-        <h1>SignupPage</h1>
+    <div className="flex min-h-[calc(100vh-64px)] items-center justify-center px-4">
+      <div className="w-full max-w-md rounded-xl border bg-white p-8 shadow-md">
+        <h1 className="mb-6 text-center text-3xl font-bold"> Create Account</h1>
+
+        <form>
+          <Input placeholder="Full Name" />
+
+          <Input type="email" placeholder="Email Address" />
+
+          <Input type="password" placeholder="Password" />
+
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
