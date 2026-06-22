7cff3063b9897198102223700d4ab9708bbba961
Anveta <nangarea2017@gmail.com>
2026-06-22 17:40:36 +0530

    [style]: modify styling of signup page

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,23 +1,37 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import signupImage from "@/assets/images/SignUp.jpg";
 
 export default function SignupPage() {
   return (
-    <div className="flex min-h-[calc(100vh-64px)] items-center justify-center px-4">
-      <div className="w-full max-w-md rounded-xl border bg-white p-8 shadow-md">
-        <h1 className="mb-6 text-center text-3xl font-bold"> Create Account</h1>
+    <div className="grid min-h-screen lg:grid-cols-2">
+      <div className="hidden lg:block">
+        <img
+          src={signupImage}
+          alt="Signup"
+          className="h-full w-full object-cover"
+        />
+      </div>
 
-        <form>
-          <Input placeholder="Full Name" />
+      <div className="bg-[#cddcf8] flex items-center justify-center p-8">
+        <div className="w-full max-w-md">
+          <div className="w-full max-w-md rounded-xl border bg-white p-8 shadow-md">
+            <h1 className="mb-6 text-center text-3xl font-bold">
+              Create Account
+            </h1>
+            <form className="space-y-4">
+              <Input placeholder="Full Name" />
 
-          <Input type="email" placeholder="Email Address" />
+              <Input type="email" placeholder="Email Address" />
 
-          <Input type="password" placeholder="Password" />
+              <Input type="password" placeholder="Password" />
 
-          <Button type="submit" className="w-full">
-            Sign Up
-          </Button>
-        </form>
+              <Button type="submit" className="w-full">
+                Sign Up
+              </Button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
